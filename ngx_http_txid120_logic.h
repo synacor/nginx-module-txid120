727d6ee36984f8a5a87042dcbb3e9d164b71093c
Topaz <topaz@ganymede>
2016-04-21 19:27:30 -0400

    create gh-pages branch

--- ngx_http_txid120_logic.h
+++
@@ -1,9 +0,0 @@
-#include <time.h>
-#include <sys/time.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdint.h>
-
-void ngx_http_txid120_logic(FILE* urandom, uint8_t* txid120);
